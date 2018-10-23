49fb4ac44e16b36df3344b1e18c957db14690b57
Juan P <pront23@gmail.com>
2018-10-22 18:12:37 -0700

    Routed all the disatster components. Added HTML and CSS to each page.

--- disaster/src/app/app-routing.module.ts
+++ disaster/src/app/app-routing.module.ts
@@ -9,11 +9,26 @@ import { BagComponent } from './bag/bag.component';
 import { DescriptionComponent } from './description/description.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
+// New feature add by DevPerezJR
+import { EarthquakeComponent } from './earthquake/earthquake.component';
+import { FireComponent } from './fire/fire.component';
+import { FloodComponent } from './flood/flood.component';
+import { NuclearComponent } from './nuclear/nuclear.component';
+import { TornadoComponent } from './tornado/tornado.component';
+// New feature add by DevPerezJR
+
+
 const routes: Routes = [
   {path: 'welcome', component: WelcomeComponent},
   {path: 'disaster/home', component: HomeComponent},
   {path: 'disaster/checklist', component: ChecklistComponent},
   {path: 'disaster/description', component: DescriptionComponent},
+  {path: 'disaster/earthquake', component: EarthquakeComponent},
+  {path: 'disaster/fire', component: FireComponent},
+  {path: 'disaster/flood', component: FloodComponent},
+  {path: 'disaster/nuclear', component: NuclearComponent},
+  {path: 'disaster/tornado', component: TornadoComponent},
+  {path: 'disaster/earthquake', component: EarthquakeComponent},
   {path: 'disaster/bag', component: BagComponent},
   { path: '', pathMatch: 'full', redirectTo: '/welcome'}
 ];
