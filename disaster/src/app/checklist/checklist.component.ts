6ce19feff67da64891fa09ac1bc329d945c82fbe
Juan P <pront23@gmail.com>
2019-03-27 11:15:49 +0900

    Added ng validations to the add custom options on the checklist page. Added media queries to solve responsive issues on the welcome page. Solved the issue with the Disaster Descriptions nav bar dropdown not operating in iOS.

--- disaster/src/app/checklist/checklist.component.ts
+++ disaster/src/app/checklist/checklist.component.ts
@@ -95,81 +95,16 @@ export class ChecklistComponent implements OnInit {
         "item": "Keys",
         "weight": 0
       },
-      // {
-      //   "item": "Original Paintings",
-      //   "weight": 200
-      // },
-      // {
-      //   "item": "Non-perishable, ready-to-eat food",
-      //   "weight": 50
-      // },
-      // {
-      //   "item": "Sanitary Devices",
-      //   "weight": 15
-      // },
-      // {
-      //   "item": "Water",
-      //   "weight": 10
-      // },
-      // {
-      //   "item": "Change of clothing",
-      //   "weight": 14
-      // },
-      // {
-      //   "item": "Coats/Jackets",
-      //   "weight": 10
-      // },
-      // {
-      //   "item": "Toiletries",
-      //   "weight": 5
-      // },
-      // {
-      //   "item": "Flashlight",
-      //   "weight": 2
-      // },
-      // {
-      //   "item": "Shoes/Boots",
-      //   "weight": 7
-      // },
-      // {
-      //   "item": "Sleeping Bag",
-      //   "weight": 5
-      // },
+    
     ]
   };
     constructor(private _httpService: HttpService, private _router: Router) { }
 
     ngOnInit() {
-      // this.newBag = {item: '', weight: ''};
-      // <--This is where the form data is picked up. MAKE SURE THEY MATCH YOUR MODELS!!!!-->
     }
     addBag() {
       this.temp = Object.assign({},this.newBag);
       this.plainAdds2.push(this.temp);
-      // console.log('newBag before', this.newBag);
-
-      // for (var i=0; i<this.plainAdds.length;i++){
-      //   this.plainAdds2.push(this.plainAdds[i]);
-      // }
-      // this.plainAdds2 = Object.assign(this.newBag);
-      // console.log('plainAdds2 before', this.plainAdds2);
-      // this.plainAdds.push(this.newBag);
-      
-
-
-
-      // this.plainAdds2 = this.plainAdds;
-      // console.log('plainAdds:',this.plainAdds);
-      // this.sending();
-
-
-      // this._httpService.createBag(this.newBag).subscribe(data => {
-      //   console.log('API RESPONSE BABY!', data);
-      //   console.log(data);
-      //   this.newBag = {item: '', weight: ''};
-      //   this._router.navigate(['/disaster/bag']); // <--send them back to the main page-->
-
-      // });
     }
 
     sendCustom() {
@@ -188,23 +123,12 @@ export class ChecklistComponent implements OnInit {
     sending() {
 
       this.myConcat = this.plainCheck.concat(this.plainCheck2);
-
-      // this.myarray = this.plainCheck;
-      // for (var i = 0; i<this.plainAdds.length;i++){
-      //   this.myarray.push(this.plainAdds[i]);
-      // }
-      // console.log('after concat', this.myarray);
-      // console.log('joint array', this.myarray);
       this.myjson = {"myBag": this.myConcat};
       console.log('myjson:', this.myjson);
       var tempObservable = this._httpService.SerSendWholeBag(this.myjson);
       tempObservable.subscribe((data:any)=>{
-        // this.wholeBag = data;
         console.log('we got this from backend:', data);
         this.returnbag = data;
-        // if(!data.errors){
-        //   this._router.navigate(['/disaster/checklist']);
-        // }
       })
     }
     sendWholeBag() {
@@ -218,11 +142,4 @@ export class ChecklistComponent implements OnInit {
       }
       this.sending();
     }
-    // testfunction(){
-    //   console.log('testbag:', this.testbag);
-    // }
-    // trackByIdx(index: number, obj: any): any {
-    //   return index;
-    // }
-
 }
